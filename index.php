a672acbf6934f76fa69d79085ffeb498265bc5d6
Martin Anduaga <martinanduaga9@gmail.com>
2021-11-24 14:35:00 -0700

    Actualizando archivos a git

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   
+  </head>
+  <body>
+    <h2 style="color: #2e6c80;">Mi pagina en Heroku:</h2>
+<p>Mi nombre es Martin Anduaga, de Hermosillo Sonora</p>
+<p>Comparto una imagen de mi ciudad</p>
+<p><img src="http://visitsonora.mx/wp-content/uploads/hermosillo-de-noche.jpg" alt="" /></p>
+<p>&nbsp;</p>
+<p><strong>Probando servicios en Heroku</strong></p>
+<p><strong>&nbsp;</strong></p>
+  </body>
+</html>
